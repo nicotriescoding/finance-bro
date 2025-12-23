d4464fdf04d4616835b1deb18c22586759390b9c
nicotriescoding <nicolas.dumpe@gmx.de>
2025-12-23 20:25:08 +0100

    fix usePrevious hook typing

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,10 +1,11 @@
-// src/hooks/usePrevious.ts
 import { useEffect, useRef } from "react";
 
 export function usePrevious<T>(value: T): T | undefined {
-    const ref = useRef<T>();
+    const ref = useRef<T | undefined>(undefined);
+
     useEffect(() => {
         ref.current = value;
     }, [value]);
+
     return ref.current;
 }
