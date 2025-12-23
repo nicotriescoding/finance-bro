e22334c7d2579fa90dc898ae6ee1a31f5a313ca0
nicotriescoding <nicolas.dumpe@gmx.de>
2025-12-23 20:45:41 +0100

    fix bond formulas typing

--- src/lib/formulas/bonds.ts
+++ src/lib/formulas/bonds.ts
@@ -1,31 +1,79 @@
-// src/lib/formulas/bonds.ts
+// --- Typdefinitionen ---
+
+type ZeroBond = {
+    BN: number;
+    r: number;
+    N: number;
+};
+
+type CouponBond = {
+    C: number;
+    BN: number;
+    r: number;
+    N: number;
+};
+
+type BondDuration = {
+    B0: number;
+    Ck: number;
+    BN: number;
+    r: number;
+    N: number;
+};
+
+type ModifiedDuration = {
+    D: number;
+    r: number;
+};
+
+type PriceChangeApprox = {
+    Dmod: number;
+    dr: number;
+};
+
+type ForwardRate = {
+    It_T: number;
+    It_S: number;
+    T: number;
+    S: number;
+    t: number;
+};
+
+// --- Formeln ---
 
 export const bondFormulas = {
     // --- BASIC BOND VALUATION ---
-    zerobond_price: ({ BN, r, N }) => BN / Math.pow(1 + r / 100, N),
+    zerobond_price: ({ BN, r, N }: ZeroBond) =>
+        BN / Math.pow(1 + r / 100, N),
 
-    coupon_bond_price: ({ C, BN, r, N }) =>
+    coupon_bond_price: ({ C, BN, r, N }: CouponBond) =>
         (C * ((Math.pow(1 + r / 100, N) - 1) / (r / 100)) + BN) /
         Math.pow(1 + r / 100, N),
 
     // --- DURATION & PRICE SENSITIVITY ---
-    bond_duration_D: ({ B0, Ck, BN, r, N }) =>
+    bond_duration_D: ({ B0, Ck, BN, r, N }: BondDuration) =>
         (1 / B0) *
         ((Ck * N) / Math.pow(1 + r / 100, N) +
             (N * BN) / Math.pow(1 + r / 100, N)),
 
-    bond_modified_duration: ({ D, r }) => D / (1 + r / 100),
+    bond_modified_duration: ({ D, r }: ModifiedDuration) =>
+        D / (1 + r / 100),
 
-    bond_price_change_approx: ({ Dmod, dr }) => -Dmod * dr,
+    bond_price_change_approx: ({ Dmod, dr }: PriceChangeApprox) =>
+        -Dmod * dr,
 
     // --- TERM STRUCTURE (FORWARD RATES) ---
-    term_structure_forward_rate: ({ It_T, It_S, T, S, t }) => {
-        // (1 + rS_T)^(T-S) = (1 + It_T)^(T-t) / (1 + It_S)^(S-t)
-        // => rS_T = ((1 + It_T)^(T-t) / (1 + It_S)^(S-t))^(1/(T-S)) - 1
+    term_structure_forward_rate: ({
+                                      It_T,
+                                      It_S,
+                                      T,
+                                      S,
+                                      t,
+                                  }: ForwardRate) => {
         return (
             Math.pow(
-                (Math.pow(1 + It_T / 100, T - t) /
-                    Math.pow(1 + It_S / 100, S - t)),
+                Math.pow(1 + It_T / 100, T - t) /
+                Math.pow(1 + It_S / 100, S - t),
                 1 / (T - S)
             ) - 1
         );
