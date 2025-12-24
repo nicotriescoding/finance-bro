49a465bfb6d4fffa77b8b82b339ad3360ffd906e
nicotriescoding <nicolas.dumpe@gmx.de>
2025-12-25 00:54:25 +0100

    fix: clean next.config (remove eslint & duplicate export)

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-        // Ignore ESLint errors during production builds
-        ignoreDuringBuilds: true,
-    },
-};
-
-module.exports = nextConfig;
